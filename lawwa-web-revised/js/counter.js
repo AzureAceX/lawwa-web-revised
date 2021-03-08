f1ee4d53b8e7ddfd02a1224d7217769670145459
AzureAceXX <Urillah@gmail.com>
2021-03-08 17:12:38 +0800

    updates

--- lawwa-web-revised/js/counter.js
+++ lawwa-web-revised/js/counter.js
@@ -1,17 +1,17 @@
 function updateCounter(){
-    fetch('https://api.countapi.xyz/update/LawwaWebsiteVistors/visitor/?amount=1')
+    fetch('https://api.countapi.xyz/update/LawwaWebsiteVistorCount/visitosr/?amount=+1')
     .then(res => res.json())
-    .then(data => counterDisplay.innerHTML = data.value)
+    .then(data => counterDisplay.innerHTML = "Visitors:" + data.value)
 }
 
 updateCounter()
-
 
 counterDisplay = document.getElementsByClassName('counterDisplay')[0];
 
 /*
 //creating namespace
-https://api.countapi.xyz/create?namespace=LawwaWebsiteVistors&key=visitor&value=0
+// OLD KEY https://api.countapi.xyz/create?namespace=LawwaWebsiteVistors&key=visitor&value=0
+https://api.countapi.xyz/create?namespace=LawwaWebsiteVistorCount&key=visitosr&value=0&enable_reset=1
 
 //get namespace value
 https://api.countapi.xyz/get/LawwaWebsiteVistors/visitor
